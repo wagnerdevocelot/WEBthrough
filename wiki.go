fee7b9cc1ebf4412c1f1fb5721f22f665e574486
Wagner Abrantes <wagner.dev1337@gmail.com>
2020-08-09 19:43:04 -0300

    estrutura de dados

--- wiki.go
+++ wiki.go
@@ -1,7 +1,18 @@
 package main
 
-// O inicio de todo arquivo Go inicia com a definição do pacote main e da função main
-// simples assim!
+// Vamos começar definindo as estruturas de dados. Um wiki consiste em uma série de páginas interconectadas,
+// cada uma com um título e um corpo (o conteúdo da página). Aqui, definimos Pagina como uma estrutura com dois
+// campos representando o título e o corpo.
+
+// Pagina é um struct com a estrutura que as páginas da aplicação terão
+type Pagina struct {
+	Titulo string
+	Corpo  []byte
+}
+
+// O tipo []byte significa "um byte slice".
+// Consulte slices https://golang.org/doc/articles/slices_usage_and_internals.html para obter mais informações
+// O elemento Corpo é um []byte e não string porque esse é o tipo esperado pelos pacotes de io que usaremos.
 
 func main() {
 
