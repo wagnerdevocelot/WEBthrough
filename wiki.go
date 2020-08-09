019e4720cdefcc6c9990474afb1783aba57df5f4
Wagner Abrantes <wagner.dev1337@gmail.com>
2020-08-09 20:13:40 -0300

    persistência

--- wiki.go
+++ wiki.go
@@ -1,8 +1,7 @@
 package main
 
-// Vamos começar definindo as estruturas de dados. Um wiki consiste em uma série de páginas interconectadas,
-// cada uma com um título e um corpo (o conteúdo da página). Aqui, definimos Pagina como uma estrutura com dois
-// campos representando o título e o corpo.
+// importação do pacote io/ioutil
+import "io/ioutil"
 
 // Pagina é um struct com a estrutura que as páginas da aplicação terão
 type Pagina struct {
@@ -10,9 +9,26 @@ type Pagina struct {
 	Corpo  []byte
 }
 
-// O tipo []byte significa "um byte slice".
-// Consulte slices https://golang.org/doc/articles/slices_usage_and_internals.html para obter mais informações
-// O elemento Corpo é um []byte e não string porque esse é o tipo esperado pelos pacotes de io que usaremos.
+// A struct Pagina descreve como os dados da página serão armazenados na memória.
+// Mas e o armazenamento persistente? Podemos resolver isso criando um método salvar:
+
+func (p *Pagina) salvar() error {
+	nomeDoArquivo := p.Titulo + ".txt"
+	return ioutil.WriteFile(nomeDoArquivo, p.Corpo, 0600)
+}
+
+// O que o método salvar faz? Ele tem um receiver que aponta para Pagina e tem um argumento "p"
+// salvar não possui parametros além do presente no receiver, e retorna um error.
+
+// nomeDoArquivo é uma varável que armazena o valor, Titulo + a string ".txt" que é a extenção
+
+// O retorno de salvar é um error pois esse é o mesmo tipo que WriteFile retorna.
+// O que WriteFile faz? WriteFile escreve um slice de bytes em um arquivo se salvar funcionar sem erros
+// ele retorna nil que é o zero value para pointers.
+
+// O literal inteiro 0600, passado como o terceiro parâmetro para WriteFile, indica que o arquivo
+// deve ser criado com permissões de leitura e gravação apenas para o usuário atual.
+// (Veja a página de manual do Unix open(2) para mais detalhes.)
 
 func main() {
 
