3b9f8b2edeb8122eb30ca70f86a3d488bf43f2b1
Wagner Abrantes <wagner.dev1337@gmail.com>
2020-08-11 14:39:11 -0300

    salvando paginas

--- wiki.go
+++ wiki.go
@@ -35,14 +35,6 @@ func carregaPagina(titulo string) (*Pagina, error) {
 	return &Pagina{Titulo: titulo, Corpo: corpo}, nil
 }
 
-// E se você visitar /view/umaPaginaQueNaoExiste ?
-// Você verá uma página contendo HTML. Isso ocorre porque ele ignora o valor de retorno do
-// erro e continua tentando preencher o template sem dados. Em vez disso, se a página solicitada não existir,
-// ele deve redirecionar o cliente para a página de edição para que o conteúdo possa ser criado:
-
-// A função http.Redirect adiciona um código de status HTTP de http.StatusFound (302) e um Location
-// header à resposta HTTP.
-
 // viewHandler r o titulo e corpo da pagina em html formatado
 func viewHandler(escrever http.ResponseWriter, ler *http.Request) {
 	titulo := ler.URL.Path[len("/view/"):]
@@ -64,9 +56,30 @@ func editHandler(escrever http.ResponseWriter, ler *http.Request) {
 	renderizaTemplate(escrever, "edit", pagina)
 }
 
+// O título da página (fornecido no URL) e campo do formulário, body são armazenados em uma nova Pagina.
+// O método salvar() é então chamado para gravar os dados em um arquivo e o cliente é
+// redirecionado para a /view/. O valor retornado por FormValue é do tipo string.
+// Devemos converter esse valor para []byte antes que ele caiba na struct Pagina. Usamos []byte(corpo)
+// para realizar a conversão.
+
+// A função saveHandler tratará do envio de formulários localizados nas páginas de edição
+func saveHandler(escrever http.ResponseWriter, ler *http.Request) {
+	titulo := ler.URL.Path[len("/save/"):]
+	corpo := ler.FormValue("body")
+	pagina := &Pagina{Titulo: titulo, Corpo: []byte(corpo)}
+	pagina.salvar()
+	http.Redirect(escrever, ler, "/view/"+titulo, http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	//	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+// para testar basta executar buildar o código novamente, executar ./wiki e abrir o browser na porta 8080
+
+// O path em view e em seguida adicione um caminho que não esxiste, assim: http://localhost:8080/view/vapordev
+// Seremos redirecionados para a pagina de Editando vapordev e quando salvar o conteudo da pagina estará
+// em um arquivo .txt com o titulo tendo o nome do path e o conteudo com o corpo da edição.
